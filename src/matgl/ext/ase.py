73a775cea41c975e441a2236fff90100f898a562
Shyue Ping Ong <shyuep@gmail.com>
2026-03-05 20:29:19 +0800

    Fix missing M3GNetCalculator.

--- src/matgl/ext/ase.py
+++ src/matgl/ext/ase.py
@@ -5,15 +5,32 @@ from __future__ import annotations
 import matgl
 
 if matgl.config.BACKEND == "DGL":
-    from ._ase_dgl import OPTIMIZERS, Atoms2Graph, MolecularDynamics, PESCalculator, Relaxer, TrajectoryObserver
+    from ._ase_dgl import (
+        OPTIMIZERS,
+        Atoms2Graph,
+        M3GNetCalculator,
+        MolecularDynamics,
+        PESCalculator,
+        Relaxer,
+        TrajectoryObserver,
+    )
 else:
     from ._ase_pyg import (  # type: ignore[assignment]
         OPTIMIZERS,
         Atoms2Graph,
+        M3GNetCalculator,
         MolecularDynamics,
         PESCalculator,
         Relaxer,
         TrajectoryObserver,
     )
 
-__all__ = ["OPTIMIZERS", "Atoms2Graph", "MolecularDynamics", "PESCalculator", "Relaxer", "TrajectoryObserver"]
+__all__ = [
+    "OPTIMIZERS",
+    "Atoms2Graph",
+    "M3GNetCalculator",
+    "MolecularDynamics",
+    "PESCalculator",
+    "Relaxer",
+    "TrajectoryObserver",
+]
